329d8179c2b2f2e0ee1c3fa67918f46779acc3f9
John Doe <abdullahalmasrur8@gmail.com>
2024-06-16 00:01:57 +0600

    #

--- profile.php
+++ profile.php
@@ -7,7 +7,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Profile</title>
     <!-- <link rel="stylesheet" href="css/style.css"> -->
     <link rel="stylesheet" href="css/next.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -93,7 +93,6 @@ session_start();
         .hidden {
             display: none;
         }
-
     </style>
 </head>
 
@@ -136,11 +135,13 @@ session_start();
                     <div class="card" id="pinfo" style="border-radius: .5rem;">
                         <form action="" method="POST">
                             <div class="row g-0">
-                                <div class="col-md-4 gradient-custom text-center text-white"
-                                    style="border-top-left-radius: .5rem; border-bottom-left-radius: .5rem;">
-                                    <img src="images/blank-profile-picture.jpg" alt="Avatar" class="img-fluid my-5"
-                                        style="width: 80px;" />
-                                    <h5>Name</h5>
+                                <div class="col-md-4 gradient-custom text-center text-white d-flex justify-content-center align-items-center"
+                                    style="height: auto;">
+                                    <div>
+                                        <img src="images/blank-profile-picture.jpg" alt="Avatar" class="img-fluid mb-4"
+                                            style="width: 10rem;" />
+                                        <h5>Full Name</h5>
+                                    </div>
                                 </div>
                                 <div class="col-md-8">
                                     <div class="card-body p-4">
@@ -185,17 +186,37 @@ session_start();
                                                 <p class="text-muted">info@example.com</p>
                                             </div>
                                             <div class="col-6 mb-3">
+                                                <h6>Nationality</h6>
+                                                <p class="text-muted">info@example.com</p>
+                                            </div>
+                                        </div>
+                                        <div class="row pt-1">
+                                            <div class="col-6 mb-3">
                                                 <h6>Address</h6>
+                                                <p class="text-muted">123 456 789</p>
+                                            </div>
+                                            <div class="col-6 mb-3">
+                                                <h6>Postcode</h6>
                                                 <p class="text-muted">123 456 789</p>
                                             </div>
                                         </div>
                                         <div class="row pt-1">
                                             <div class="col-6 mb-3">
-                                                <h6>Country</h6>
-                                                <p class="text-muted">info@example.com</p>
+                                                <h6>Passport Number</h6>
+                                                <p class="text-muted">123 456 789</p>
                                             </div>
                                             <div class="col-6 mb-3">
-                                                <h6>Gender</h6>
+                                                <h6>Issue Country</h6>
+                                                <p class="text-muted">123 456 789</p>
+                                            </div>
+                                        </div>
+                                        <div class="row pt-1">
+                                            <div class="col-6 mb-3">
+                                                <h6>Issue Date</h6>
+                                                <p class="text-muted">123 456 789</p>
+                                            </div>
+                                            <div class="col-6 mb-3">
+                                                <h6>Expiry Date</h6>
                                                 <p class="text-muted">123 456 789</p>
                                             </div>
                                         </div>
@@ -214,11 +235,13 @@ session_start();
                     <div class="card hidden" id="wexp" style="border-radius: .5rem;">
                         <form action="" method="POST">
                             <div class="row g-0">
-                                <div class="col-md-4 gradient-custom text-center text-white"
-                                    style="border-top-left-radius: .5rem; border-bottom-left-radius: .5rem;">
-                                    <img src="images/blank-profile-picture.jpg" alt="Avatar" class="img-fluid my-5"
-                                        style="width: 80px;" />
-                                    <h5>Name</h5>
+                                <div class="col-md-4 gradient-custom text-center text-white d-flex justify-content-center align-items-center"
+                                    style="height: auto;">
+                                    <div>
+                                        <img src="images/blank-profile-picture.jpg" alt="Avatar" class="img-fluid mb-4"
+                                            style="width: 10rem;" />
+                                        <h5>Full Name</h5>
+                                    </div>
                                 </div>
                                 <div class="col-md-8">
                                     <div class="card-body p-4">
@@ -292,11 +315,13 @@ session_start();
                     <div class="card hidden" id="equalif" style="border-radius: .5rem;">
                         <form action="" method="POST">
                             <div class="row g-0">
-                                <div class="col-md-4 gradient-custom text-center text-white"
-                                    style="border-top-left-radius: .5rem; border-bottom-left-radius: .5rem;">
-                                    <img src="images/blank-profile-picture.jpg" alt="Avatar" class="img-fluid my-5"
-                                        style="width: 80px;" />
-                                    <h5>Name</h5>
+                                <div class="col-md-4 gradient-custom text-center text-white d-flex justify-content-center align-items-center"
+                                    style="height: auto;">
+                                    <div>
+                                        <img src="images/blank-profile-picture.jpg" alt="Avatar" class="img-fluid mb-4"
+                                            style="width: 10rem;" />
+                                        <h5>Full Name</h5>
+                                    </div>
                                 </div>
                                 <div class="col-md-8">
                                     <div class="card-body p-4">
@@ -307,51 +332,41 @@ session_start();
                                         <hr class="mt-0 mb-4">
                                         <div class="row pt-1">
                                             <div class="col-6 mb-3">
-                                                <h6>First Name</h6>
+                                                <h6>Name of the Institution</h6>
                                                 <p class="text-muted">info@example.com</p>
                                             </div>
                                             <div class="col-6 mb-3">
-                                                <h6>Last Name</h6>
+                                                <h6>Country of Study</h6>
                                                 <p class="text-muted">123 456 789</p>
                                             </div>
                                         </div>
                                         <div class="row pt-1">
                                             <div class="col-6 mb-3">
-                                                <h6>Email</h6>
+                                                <h6>Qualification Achieved</h6>
                                                 <p class="text-muted">info@example.com</p>
                                             </div>
                                             <div class="col-6 mb-3">
-                                                <h6>Phone Number</h6>
+                                                <h6>CGPA</h6>
                                                 <p class="text-muted">123 456 789</p>
                                             </div>
                                         </div>
                                         <div class="row pt-1">
                                             <div class="col-6 mb-3">
-                                                <h6>Date of Birth</h6>
+                                                <h6>Start Date</h6>
                                                 <p class="text-muted">info@example.com</p>
                                             </div>
                                             <div class="col-6 mb-3">
-                                                <h6>Gender</h6>
+                                                <h6>End Date</h6>
                                                 <p class="text-muted">123 456 789</p>
                                             </div>
                                         </div>
                                         <div class="row pt-1">
                                             <div class="col-6 mb-3">
-                                                <h6>Martial Status</h6>
+                                                <h6>Primary Language</h6>
                                                 <p class="text-muted">info@example.com</p>
                                             </div>
                                             <div class="col-6 mb-3">
                                                 <h6>Address</h6>
-                                                <p class="text-muted">123 456 789</p>
-                                            </div>
-                                        </div>
-                                        <div class="row pt-1">
-                                            <div class="col-6 mb-3">
-                                                <h6>Country</h6>
-                                                <p class="text-muted">info@example.com</p>
-                                            </div>
-                                            <div class="col-6 mb-3">
-                                                <h6>Gender</h6>
                                                 <p class="text-muted">123 456 789</p>
                                             </div>
                                         </div>

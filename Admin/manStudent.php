de6e40bdbe2438a54274599f3aef031b1436f152
John Doe <abdullahalmasrur8@gmail.com>
2024-09-03 20:50:04 +0600

    session fixed

--- Admin/manStudent.php
+++ Admin/manStudent.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
-
+if (!isset($_SESSION['user']) || $_SESSION['user'] !== 'admin') {
+    echo "<script>alert('You have to Login First!!!')</script>";
+    echo "<script>location.href='../login.php'</script>";
+}
 include "../connection.php";
 
 ?>

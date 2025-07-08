a5984f79c6097bdd53b4157e9e655ca73f70b428
Root <abdullahalmasrur8@gmail.com>
2025-07-08 23:33:26 +0600

    #

--- sendmail.php
+++ sendmail.php
@@ -13,19 +13,21 @@ $mail = new PHPMailer(true);
 
 //Server settings
 $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-$mail->isSMTP();
-//Send using SMTP
-$mail->Host = 'mail.nextsteps.study'; //Set the SMTP server to send through
-$mail->SMTPAuth = true;
-$mail->Username = 'noreply@nextsteps.study'; //SMTP username
-$mail->Password = 'rOka6SDX#O^{'; //SMTP password
+$mail->isSMTP();                                            //Send using SMTP
+
+$mail->Host       = 'server161.web-hosting.com';                     //Set the SMTP server to send through
+$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+$mail->Username   = 'email@mynextsteps.co';                     //SMTP username
+$mail->Password   = 'm}nnHbcVd1BP';                               //SMTP password
 
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
 $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
 //Recipients
-$mail->setFrom('noreply@nextsteps.study', 'Nextsteps');
-$mail->addAddress($reg_email); //Add a recipient
+$mail->setFrom('email@mynextsteps.co', 'Nextsteps');
+
+$mail->addAddress($reg_email);               //Name is optional
+
 
 //Content
 $mail->isHTML(true); //Set email format to HTML
--- verifytoken.php
+++ verifytoken.php
@@ -11,11 +11,11 @@ if (isset($_GET['token'])) {
     if (mysqli_num_rows($verify_query) > 0) {
         $row = mysqli_fetch_assoc($verify_query);
         // echo $row['verifytoken'];
-        if ($row['verifystatus'] == 0) {
-            $update_query = mysqli_query($conn, "UPDATE auth SET verifyStatus = '1' WHERE verifyToken = '{$row['verifytoken']}'");
+        if ($row['verifyStatus'] == 0) {
+            $update_query = mysqli_query($conn, "UPDATE auth SET verifyStatus = '1' WHERE verifyToken = '{$row['verifyToken']}'");
             if ($update_query) {
                 echo "<script>alert('Account Has Been Verified Successfully. Please Login.!')</script>";
-                echo "<script>location.href='login.php'</script>";
+                // echo "<script>location.href='login.php'</script>";
             } else {
                 echo "<script>alert('Verification Failed. Please Try Again!')</script>";
                 echo "<script>location.href='login.php'</script>";
